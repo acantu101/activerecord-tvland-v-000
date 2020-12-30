6a2a095d73b905d25758a1097302dcc62422b221
alejandra cantu <alejandra.cantu13@gmail.com>
2020-12-30 17:30:30 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,14 @@
 class Show < ActiveRecord::Base
 
 belongs_to :network
-has_many :actors
-has_many :characters
+has_many :actors, through: characters:
 
   def actors_list
     #that returns an Array of the full names of each actor
     # associated with the a show.
     # Remember, a show should have many actors through characters.
-    binding.pry
+
+    
 
 
 
