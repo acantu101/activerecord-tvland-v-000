778d31c6010a56762121415b9d83820493e4e57b
alejandra cantu <alejandra.cantu13@gmail.com>
2020-12-30 17:35:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
 
 belongs_to :network
-has_many :actors, through: characters:
+has_many :actors, through: :characters
 
   def actors_list
     #that returns an Array of the full names of each actor
